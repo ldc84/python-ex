2b332bdcbbe5e43f4724b6bacdd72a67ae46bf8d
unknown <Administrator@SKCC14D00871.SKCC.NET>
2020-01-30 15:52:57 +0900

    논리연산

---
+++ basic/20.bool_test.py
@@ -0,0 +1,3 @@
+value = input('입력해주세요>') or '아무것도 못받았어'
+
+print('입력받은값>',value)
---
+++ basic/20.logic.py
@@ -0,0 +1,29 @@
+# def return_false():
+#   print('함수 return_false')
+#   return False
+
+# def return_true():
+#   print('함수 return_true')
+#   return True
+
+# print('테스트1')
+# a = return_false()
+# b = return_true()
+
+# if a and b:
+#   print(True)
+# else:
+#   print(False)
+
+# print('테스트2')
+# if return_false() and return_true(): # 단락평가
+#   print('True')
+# else:
+#   print(False)
+
+dic = {'Kye2': 'value1'}
+
+if "Key1" in dic and dic['Key1'] == 'Value1':
+  print('Key1도 있고, 그 값은 Value1이네')
+else:
+  print('아니네')
